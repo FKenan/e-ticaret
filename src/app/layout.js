128287d6fb32708cd0227711f2685b4f240d76bb
FKenan <kenanf734@gmail.com>
2025-09-08 14:42:50 +0300

    feat(layout): add navbar and theme registry
    - Added Navbar component to the root layout
    - Integrated ThemeRegistry for consistent styling
    - Wrapped children with ThemeRegistry and Navbar components
    
    This commit enhances the application's layout by introducing key UI components and setting the default language to Turkish, improving accessibility and user experience for Turkish-speaking users.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import ThemeRegistry from "@/components/ThemeRegistry";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,9 +20,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="tr">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <ThemeRegistry>
+          <Navbar />
+          {children}
+        </ThemeRegistry>
       </body>
     </html>
   );
